3e15a90682621a0397bb4d45793cb4b75630c047
karla <karla.leal1988@gmail.com>
2019-09-10 16:26:17 -0300

    Ya corrio el testeo en system\

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,8 +1,14 @@
 require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
-  test 'Bisitando la pagina principal' do
+
+  test 'Revisando el titulo de la pagina principal ' do
     visit posts_path
-    assert_selector "h3", text: "hola"
+    assert_selector "h1", text: "Blog Post"
   end
+
+  test 'Revisando la creacion de un usuario' do
+    
+  end 
+
 end
