bbe23c6d6af0ba2fba8f3627cfaa1aa2fa6b4047
karla <karla.leal1988@gmail.com>
2019-09-10 17:33:59 -0300

    primer sistema

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -8,7 +8,19 @@ class PostsTest < ApplicationSystemTestCase
   end
 
   test 'Revisando la creacion de un usuario' do
+
+    visit new_post_path
+
+    fill_in 'Title', with: 'a title'
+    fill_in 'Body', with: 'a body'
+
+    click_button 'Save Post'
+
+    visit posts_path
     
+    assert_selector 'div h3', text: 'a title'
+    assert_selector 'div p', text: 'a body'
+
   end 
 
 end
